4afced783297c0f1e21e4ab1debdaf9e46a19b1d
MD Akmami <akhmami@gmail.com>
2022-10-11 08:24:30 +0700

    fixed brand table

--- resources/views/dash/brand.blade.php
+++ resources/views/dash/brand.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="pt-8 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h1 class="flex-1 text-2xl font-bold text-gray-900">Tag</h1>
+        <h1 class="flex-1 text-2xl font-bold text-gray-900">Merek</h1>
         <!-- Table -->
         <livewire:brand.table />
     </div>
