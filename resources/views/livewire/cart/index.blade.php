9e3591b9817eb1c3bc492c63942f48b7927c4e90
MD Akmami <akhmami@gmail.com>
2023-02-12 21:19:05 +0700

    fixed loading state

--- resources/views/livewire/cart/index.blade.php
+++ resources/views/livewire/cart/index.blade.php
@@ -34,9 +34,9 @@
                                 <div class="flex items-start justify-between">
                                     <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">Keranjang
                                     </h2>
-                                    <div class="ml-3 flex h-7 items-center" wire:loading.remove wire:target="checkout">
-                                        <button x-on:click="open = false" type="button"
-                                            class="-m-2 p-2 text-gray-400 hover:text-gray-500">
+                                    <div class="ml-3 flex h-7 items-center">
+                                        <button x-on:click="open = false" type="button" wire:loading.attr="disabled"
+                                            wire:target="checkout" class="-m-2 p-2 text-gray-400 hover:text-gray-500">
                                             <span class="sr-only">Close panel</span>
                                             <!-- Heroicon name: outline/x-mark -->
                                             <x-icon.o-x class="h-6 w-6" />
@@ -147,27 +147,29 @@
                                         :error="$errors->first('taken_by')" />
                                 </div>
                                 <div class="mt-6">
-                                    <a wire:click.prevent="checkout" wire:loading.remove wire:target="checkout"
-                                        href="#"
-                                        class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Checkout</a>
-                                    <x-button.primary wire:loading.flex wire:target="checkout"
-                                        class="inline-flex items-center justify-center" disabled>
-                                        <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                            <circle class="opacity-25" cx="12" cy="12" r="10"
-                                                stroke="currentColor" stroke-width="4"></circle>
-                                            <path class="opacity-75" fill="currentColor"
-                                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-                                            </path>
-                                        </svg>
-                                        Processing...
-                                    </x-button.primary>
+                                    <a wire:click.prevent="checkout" wire:loading.class.remove="hover:bg-indigo-700"
+                                        wire:loading.attr="disabled" href="#"
+                                        class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">
+                                        <span wire:loading.remove wire:target="checkout">Checkout</span>
+                                        <span wire:loading.flex wire:target="checkout">
+                                            <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                                                xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                viewBox="0 0 24 24">
+                                                <circle class="opacity-25" cx="12" cy="12"
+                                                    r="10" stroke="currentColor" stroke-width="4"></circle>
+                                                <path class="opacity-75" fill="currentColor"
+                                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                                                </path>
+                                            </svg>
+                                            Processing...
+                                        </span>
+                                    </a>
                                 </div>
-                                <div class="mt-6 flex justify-center text-center text-sm text-gray-500"
-                                    wire:loading.remove wire:target="checkout">
+                                <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
                                     <p>
                                         atau
-                                        <button x-on:click="open = false" type="button"
+                                        <button x-on:click="open = false" type="button" wire:loading.attr="disabled"
+                                            wire:target="checkout"
                                             class="font-medium text-indigo-600 hover:text-indigo-500">
                                             Tambah barang lain
                                             <span aria-hidden="true"> &rarr;</span>
