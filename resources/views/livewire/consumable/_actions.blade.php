93f533277b376e66433023bf4d60b20a8eaa122d
MD Akhmami - Komputer NF <akhmami@gmail.com>
2023-03-06 12:01:34 +0700

    button

--- resources/views/livewire/consumable/_actions.blade.php
+++ resources/views/livewire/consumable/_actions.blade.php
@@ -1,5 +1,6 @@
 <div wire:key="button-{{ $consumable->id }}" class="flex items-center justify-end space-x-4">
-    <x-button.primary wire:click="addCart({{ $consumable->id }})" class="flex whitespace-nowrap items-center bg py-1">
+    <x-button.primary wire:click="addCart({{ $consumable->id }})"
+        class="flex whitespace-nowrap items-center bg py-1 pl-2.5">
         <x-icon.o-plus class="h-4 w-4 mr-1" /> Keranjang
     </x-button.primary>
     <x-table.actions>
